99b481a9ff5f1ceb6def83cdce03b90505645f10
Delemangi <milev.stefan@gmail.com>
2026-02-23 01:49:16 +0100

    Rebrand

--- src/translations/about.ts
+++ src/translations/about.ts
@@ -1,11 +1,11 @@
 import { hyperlink, userMention } from 'discord.js';
 
-export const botName = 'ФИНКИ Discord Бот';
+export const botName = 'ФИНКИ Хаб / Дискорд Бот';
 
 export const aboutMessage = (helpCommand: string, faqCommand: string) =>
   `Овој Дискорд бот е развиен од ${userMention(
     '198249751001563136',
-  )} за потребите на ${hyperlink('ФИНКИ Хаб', 'https://finki-hub.com')}, независната студентска организација. Кодот е отворен и се наоѓа на ${hyperlink(
+  )} за потребите на ${hyperlink('ФИНКИ Хаб', 'https://finki-hub.com')}. Кодот е отворен и се наоѓа на ${hyperlink(
     'GitHub',
     'https://github.com/finki-hub/discord-bot',
   )}. Ако имате било какви прашања, предлози или проблеми, контактирајте нè на Discord или на GitHub. \n\nНапишете ${helpCommand} за да ги видите сите достапни команди, или ${faqCommand} за да ги видите сите достапни прашања.`;
